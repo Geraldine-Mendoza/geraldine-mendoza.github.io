c9f9e44157b61529e24994070aec22b4e41eddce
geraldine <hypatia.of.alexandria55@gmail.com>
2020-01-20 14:25:37 -0500

    Updates

---
+++ precache-manifest.f51522fa5c3abb09d56671e063395fb7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f1c09082a88811bc3a0210badc5580c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d004ea34ee2d62ed3ef",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "fb60e4e5acf4d83fdad6",
+    "url": "/static/css/main.a5dca678.chunk.css"
+  },
+  {
+    "revision": "3d004ea34ee2d62ed3ef",
+    "url": "/static/js/2.25232633.chunk.js"
+  },
+  {
+    "revision": "fb60e4e5acf4d83fdad6",
+    "url": "/static/js/main.2345eb51.chunk.js"
+  },
+  {
+    "revision": "10c32128b382e51151d6",
+    "url": "/static/js/runtime-main.4f89fcee.js"
+  },
+  {
+    "revision": "c62539e5da663d2cbe119e99b801f1e7",
+    "url": "/static/media/prom_profile.c62539e5.jpeg"
+  },
+  {
+    "revision": "88c2f2272f22cc27af09173157ec73c2",
+    "url": "/static/media/rectangle.88c2f227.svg"
+  }
+]);
